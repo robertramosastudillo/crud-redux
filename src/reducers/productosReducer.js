49f95c4e703478421df6a57d5b4077edacefa64e
Robert Andres Ramos Astudillo <rramos.playtechla@gmail.com>
2020-09-22 16:27:29 -0500

    Se realizo la funcion de listar los productos

--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -2,6 +2,9 @@ import {
   AGREGAR_PRODUCTO,
   AGREGAR_PRODUCTO_EXITO,
   AGREGAR_PRODUCTO_ERROR,
+  COMENZAR_DESCARGA_PRODUCTOS,
+  DESCARGA_PRODUCTOS_EXITO,
+  DESCARGA_PRODUCTOS_ERROR,
 } from "../types";
 
 // Cada reducer tiene su propio state
@@ -13,6 +16,7 @@ const initialState = {
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case COMENZAR_DESCARGA_PRODUCTOS:
     case AGREGAR_PRODUCTO:
       return {
         ...state,
@@ -26,11 +30,20 @@ export default function (state = initialState, action) {
         productos: [...state.productos, action.payload],
       };
     case AGREGAR_PRODUCTO_ERROR:
+    case DESCARGA_PRODUCTOS_ERROR:
       return {
         ...state,
         loading: false,
         error: action.payload,
       };
+    case DESCARGA_PRODUCTOS_EXITO:
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        productos: action.payload,
+      };
+
     default:
       return state;
   }
