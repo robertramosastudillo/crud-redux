011ff570c13ad6ec0739934fe3b726d1ba40b548
robertramosastudillo <robertramosastudillo@gmail.com>
2020-09-21 20:02:11 -0500

    Se comenzo con la implementacion de redux y otras librerias

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Productos from "./components/Productos";
+import NuevoProducto from "./components/NuevoProducto";
+import EditarProducto from "./components/EditarProducto";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Redux</h1>
-    </div>
+    <Router>
+      <Provider store={store}>
+        <Header />
+        <div className="container mt-5">
+          <Switch>
+            <Route exact path="/" component={Productos} />
+            <Route exact path="/productos/nuevo" component={NuevoProducto} />
+            <Route
+              exact
+              path="/productos/editar/:id"
+              component={EditarProducto}
+            />
+          </Switch>
+        </div>
+      </Provider>
+    </Router>
   );
 }
 
---
+++ src/components/EditarProducto.js
@@ -0,0 +1,48 @@
+import React, { Fragment } from "react";
+
+const EditarProducto = () => {
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Editar Producto
+            </h2>
+            <form action="">
+              <div className="form-group">
+                <label htmlFor="nombre">Nombre Producto</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre Producto"
+                  name="nombre"
+                  id="nombre"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="precio">Precio Producto</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Precio Producto"
+                  name="precio"
+                  id="precio"
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >
+                Guardar Cambios
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditarProducto;
